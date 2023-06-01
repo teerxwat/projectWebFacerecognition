8b73757b7f15937c9237f6aab30c8f836e48e51b
mOrales <teerawatpinchai14@gmail.com>
2023-06-01 23:55:39 +0700

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -3,13 +3,8 @@ import "./css.css";
 
 export default function Home() {
   return (
-    <div className="container">
-      <h1 className="custom-heading">กรอกรหัสนักศึกษา</h1>
-      <div>
-        <input type="text" />
-      </div>
-
-      <button className="button-margin">Submit</button>
-    </div>
+    <>
+      <h1>Login</h1>
+    </>
   );
 }
